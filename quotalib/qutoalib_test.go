3725b66b5f44bb11004024f2fca8d6ae93d372eb
Dhilip <dhilip.kumar.s@huawei.com>
2016-08-04 23:42:28 -0700

    Fix some bugs in the code and improve the test cases
    Improve test case and fix error handlilng in the code

--- quotalib/qutoalib_test.go
+++ quotalib/qutoalib_test.go
@@ -1,12 +1,17 @@
 package quotalib
 
 import (
-	"common/types"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"os/exec"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	//	"os/exec"
 	"testing"
+
+	//Packages specific to this project
+	"common/types"
 )
 
 func CreatJson() {
@@ -15,19 +20,19 @@ func CreatJson() {
 	q, _ := json.MarshalIndent(quota0, " ", "  ")
 	err := ioutil.WriteFile("quota0.json", q, 0644)
 	if err != nil {
-		fmt.Printf("WriteFile json Error: %tv", err)
+		fmt.Printf("WriteFile json Error: %v", err)
 	}
 	quota1 := &QuotaInfo{Role: "role1", Guarantee: []GuaranteeInfo{{"cpus", "*", ScalarInfo{2}, "SCALAR"}, {"mem", "*", ScalarInfo{2048}, "SCALAR"}}}
 	q, _ = json.MarshalIndent(quota1, " ", "  ")
 	err = ioutil.WriteFile("quota1.json", q, 0644)
 	if err != nil {
-		fmt.Printf("WriteFile json Error: %tv", err)
+		fmt.Printf("WriteFile json Error: %v", err)
 	}
 	quota2 := &QuotaInfo{Role: "federation", Guarantee: []GuaranteeInfo{{"cpus", "*", ScalarInfo{2}, "SCALAR"}, {"mem", "*", ScalarInfo{2048}, "SCALAR,"}}}
 	q, _ = json.MarshalIndent(quota2, " ", "  ")
 	err = ioutil.WriteFile("quota2.json", q, 0644)
 	if err != nil {
-		fmt.Printf("WriteFile json Error: %tv", err)
+		fmt.Printf("WriteFile json Error: %v", err)
 	}
 
 }
@@ -37,172 +42,163 @@ func Test_InitPlain(T *testing.T) {
 }
 
 //setquota with correct input
-func Test_SetQuota(T *testing.T) {
+func TestSetQuotaValid(T *testing.T) {
 	var dc typ.DC
-	dc.Endpoint = "172.31.44.22:5050"
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "A-OK")
+	}))
+
+	defer ts.Close()
+
+	dc.Endpoint = ts.URL
 	e := SetQuota(dc, "federation", "quota0.json")
 
 	if e != nil {
-		T.Error("error", e)
+		T.Fail()
 	}
 }
 
-//setquota with incorrect IP
-func Test_SetQuota_Ip(T *testing.T) {
+//SetQuota with Mesos Master not reachable
+func TestSetQuotaBadMaster(T *testing.T) {
 	var dc typ.DC
-	dc.Endpoint = "54.187.130.7:5050"
-	e := SetQuota(dc, "federation", "quota0.json")
 
-	if e != nil {
-		T.Error("error", e)
+	dc.Endpoint = "http://10.11.12.13:5050"
+	err := SetQuota(dc, "federation", "quota0.json")
+
+	if err == nil {
+		//Error cannot be nil
+		T.Fail()
+	}
+
+	if !strings.Contains(err.Error(), "unreachable host") {
+		//If its some other error then fail
+		T.Fail()
 	}
 }
 
-//setquota with the file which is not exist
-func Test_SetQuotaFile(T *testing.T) {
+func TestSetQuotaInvalidFilePath(T *testing.T) {
 	var dc typ.DC
-	dc.Endpoint = "172.31.44.22:5050"
-	e := SetQuota(dc, "federation", "q.json")
 
-	if e != nil {
-		T.Error("error", e)
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "A-OK")
+	}))
+
+	defer ts.Close()
+
+	dc.Endpoint = ts.URL
+	err := SetQuota(dc, "federation", "random.json")
+
+	if err == nil {
+		//Error cannot be nil
+		T.Fail()
+	}
+
+	if !strings.Contains(err.Error(), "The system cannot find the file") {
+		//If its some other error then fail
+		T.Fail()
 	}
 }
 
-//setquota with role which is not there in masterlist
-func Test_SetQuotaRole(T *testing.T) {
+//Test for invalid json format
+//We dont really have to supply an invalid json but we need to just simulate a error response from the server
+func TestSetQuotaInvalidFileJsonformat(T *testing.T) {
 	var dc typ.DC
-	dc.Endpoint = "172.31.44.22:5050"
-	e := SetQuota(dc, "role1", "quota1.json")
 
-	if e != nil {
-		T.Error("error", e)
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Wrong json", http.StatusInternalServerError)
+	}))
+
+	defer ts.Close()
+
+	dc.Endpoint = ts.URL
+	err := SetQuota(dc, "federation", "quota0.json")
+
+	if err == nil {
+		//Error cannot be nil
+		T.Fail()
 	}
+
+	if !strings.Contains(err.Error(), "Server returned error respone") {
+		//If its some other error then fail
+		T.Fail()
+	}
+
 }
 
-//setquota with incorrect json
-func Test_SetQuotaJ(T *testing.T) {
+func TestDelQuotaValid(T *testing.T) {
 	var dc typ.DC
-	dc.Endpoint = "172.31.44.22:5050"
-	e := SetQuota(dc, "federation", "quota2.json")
 
-	if e != nil {
-		T.Error("error", e)
-	}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "A-OK")
+	}))
 
-	removejson()
+	defer ts.Close()
+	dc.Endpoint = ts.URL
 
-}
+	err := DelQuota(dc, "federation")
 
-//getquota with correct input
-func Test_GetQuota(T *testing.T) {
-	var dc typ.DC
-	dc.Endpoint = "172.31.44.22:5050"
-	resp, e := GetQuota(dc, "federation")
-
-	if e != nil {
-		T.Error("error", e)
-	}
-	T.Log("the response is  ", resp)
-
-}
-
-func Test_GetQuota_Ip(T *testing.T) {
-	var dc typ.DC
-	dc.Endpoint = "172.31.44.10:5050"
-	resp, e := GetQuota(dc, "federation")
-
-	if e != nil {
-		T.Error("error", e)
-	}
-	T.Log("the response is  ", resp)
-
-}
-
-//getquota with role which is not there in masterlist
-func Test_GetQuotaRole(T *testing.T) {
-	var dc typ.DC
-	dc.Endpoint = "172.31.44.22:5050"
-	resp, e := GetQuota(dc, "role1")
-
-	if e != nil {
-		T.Error("error", e)
-	}
-	T.Log("the response is  ", resp)
-}
-
-//remaining resource with correct input
-func Test_RemainingQuota(T *testing.T) {
-	var dc typ.DC
-	dc.Endpoint = "172.31.44.22:5050"
-	cpu, mem, disk, e := RemainingResource(dc, "federation")
-
-	if e != nil {
-		T.Error("error", e)
-	}
-	T.Log("the cpu,mem,quota  are ", cpu, mem, disk)
-}
-
-func Test_RemainingQuota_Ip(T *testing.T) {
-	var dc typ.DC
-	dc.Endpoint = "172.31.44.10:5050"
-	cpu, mem, disk, e := RemainingResource(dc, "federation")
-
-	if e != nil {
-		T.Error("error", e)
-	}
-	T.Log("the cpu,mem,quota  are ", cpu, mem, disk)
-}
-
-//remaining resource with role which is not there in masterlist
-func Test_RemainingQuotaRole(T *testing.T) {
-	var dc typ.DC
-	dc.Endpoint = "172.31.44.22:5050"
-	cpu, mem, disk, e := RemainingResource(dc, "role1")
-
-	if e != nil {
-		T.Error("error", e)
-	}
-	T.Log("the cpu,mem,disk  are ", cpu, mem, disk)
-
-}
-
-//delete quota with correct input
-func Test_DelQuota(T *testing.T) {
-	var dc typ.DC
-	dc.Endpoint = "172.31.44.22:5050"
-	e := DelQuota(dc, "federation")
-
-	if e != nil {
-		T.Error("error", e)
-	}
-}
-
-//delete quota with role which is not there in masterlist
-func Test_DelQuotaRole(T *testing.T) {
-	var dc typ.DC
-	dc.Endpoint = "172.31.44.22:5050"
-	e := DelQuota(dc, "role1")
-
-	if e != nil {
-		T.Error("error", e)
-	}
-}
-
-//delete quota with incorrect master_ip
-func Test_DelQuota_Ip(T *testing.T) {
-	var dc typ.DC
-	dc.Endpoint = "172.31.44.10:5050"
-	e := DelQuota(dc, "federation")
-
-	if e != nil {
-		T.Error("error", e)
-	}
-}
-
-func removejson(){
-	_, err := exec.Command("rm", "quota0.json", "quota1.json", "quota2.json").Output()
 	if err != nil {
-		fmt.Println(err)
+		//no error should occur
+		T.Fail()
 	}
+}
+
+func TestDelQuotaInvalidResponse(T *testing.T) {
+	var dc typ.DC
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Wrong quota name", http.StatusInternalServerError)
+	}))
+	defer ts.Close()
+
+	dc.Endpoint = ts.URL
+
+	err := DelQuota(dc, "federation")
+
+	if err == nil {
+		//Should return an error for invalid server response
+		T.Fail()
+	}
+
+	if !strings.Contains(err.Error(), "Server returned error respone") {
+		//If its some other error then fail
+		T.Fail()
+	}
+}
+
+func TestDelQuotaInvalidMaster(T *testing.T) {
+	var dc typ.DC
+
+	dc.Endpoint = "https://10.11.12.13:5050"
+	err := DelQuota(dc, "federation")
+
+	if err == nil {
+		//Should return an error for invalid server response
+		T.Fail()
+	}
+
+	if !strings.Contains(err.Error(), "unreachable host") {
+		//If its some other error then fail
+		T.Fail()
+	}
+}
+
+func TestGetQuotaValid(T *testing.T) {
+	var dc typ.DC
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "A-OK")
+	}))
+
+	defer ts.Close()
+	dc.Endpoint = ts.URL
+
+	_, err := GetQuota(dc, "federation")
+
+	if err != nil {
+		//no error should occur
+		T.Fail()
+	}
+
 }
